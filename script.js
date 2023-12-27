b409164660348a7f1311ac86bd7d2a48a24eb4ad
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-12-26 22:37:46 -0500

    Add row of 16 squares

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ outerContainerDOM.style.display = "flex";
 const bodyDOM = document.querySelector("body");
 bodyDOM.append(outerContainerDOM);
 
+for (let j = 0; j < 16; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.flex = 1;
+    outerContainerDOM.append(square);
+}
