3737e59a6fc5ca61c20b8eca3a6163ed907d95e8
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-12-26 22:59:10 -0500

    Create grid using flexbox

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const outerContainerDOM = document.createElement("div");
 outerContainerDOM.classList.add("container");
 outerContainerDOM.id = "outer-container";
 outerContainerDOM.style.display = "flex";
+outerContainerDOM.style.flexDirection = "column";
 
 const bodyDOM = document.querySelector("body");
 bodyDOM.append(outerContainerDOM);
 
-for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.flex = 1;
-    outerContainerDOM.append(square);
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    row.style.flex = 1;
+    row.style.display = "flex";
+    outerContainerDOM.append(row);
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.style.flex = 1;
+        square.classList.add("square");
+        row.append(square);
+    }
 }
