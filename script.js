89074dcf27aa20bbdfce14b4db28297818f4add7
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-12-26 23:54:47 -0500

    Refactor sequences into functions

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-const outerContainerDOM = document.createElement("div");
-outerContainerDOM.classList.add("container");
-outerContainerDOM.id = "outer-container";
-outerContainerDOM.style.display = "flex";
-outerContainerDOM.style.flexDirection = "column";
-
+// CONSTANTS
+const containerDOM = document.createElement("div");
 const bodyDOM = document.querySelector("body");
-bodyDOM.append(outerContainerDOM);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.style.flex = 1;
-    row.style.display = "flex";
-    outerContainerDOM.append(row);
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.style.flex = 1;
-        square.classList.add("square");
-        row.append(square);
+
+createContainer();
+createGrid(16);
+addHoverEventListener();
+
+// FUNCTIONS
+function addHoverEventListener() {
+    squares = containerDOM.querySelectorAll(".square");
+    for (const square of squares) {
+        square.addEventListener("mouseover", fillSquare);
     }
 }
 
-squares = outerContainerDOM.querySelectorAll(".square");
-for (const square of squares) {
-    square.addEventListener("mouseover", fillSquare);
+function createContainer() {
+    containerDOM.classList.add("container");
+    containerDOM.id = "container";
+    containerDOM.style.display = "flex";
+    containerDOM.style.flexDirection = "column";
+    bodyDOM.append(containerDOM);
+}
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.flex = 1;
+        row.style.display = "flex";
+        containerDOM.append(row);
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.style.flex = 1;
+            square.classList.add("square");
+            row.append(square);
+        }
+    }
 }
 
 function fillSquare(e) {
